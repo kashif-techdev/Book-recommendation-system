f3cb165301669f07c5ebbd990d3113bc092aa3c8
Kashif <kashif.techdev@gmail.com>
2026-04-17 19:53:15 +0500

    getting ready the backend and frontend for render and vercel deployment

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"1NXPtXAPclpHbWduiwFPF9F+ohYiUQx8kBHFqb/ZjDc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"f+VNcbp2XHeRI2iOfwhmtJLYRpcUd1QqQoh3GqeL7IY=\"}"
